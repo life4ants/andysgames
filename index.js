e0d46ce382b6dd17215af3aad94fb09e8d7a3917
Andy Buck <brotherandy@live.com>
2019-05-22 23:16:56 -0400

    uncommited changes

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 
 let express = require('express')
-const app = express()
 const PORT = process.env.PORT || 5000
+const app = express();
 
-app.use(express.static("pages"))
+app.use(express.static('pages'))
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname+'/pages/index.html');
+});
+app.get('/aletterfromtheking', function (req, res) {
+    res.sendFile(__dirname+'/pages/aletterfromtheking/index.html');
+});
+app.get('/matchgame', function (req, res) {
+    res.sendFile(__dirname+'/pages/matchgame/index.html');
+});
+
+//app.use(express.static("pages"))
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
