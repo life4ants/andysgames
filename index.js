34bf1d8a1e2ad45058ece1fa3ed5b84c6166b0d0
Andy Buck <brotherandy@live.com>
2021-10-01 16:53:01 -0400

    rename pages so routes can be logged

--- index.js
+++ index.js
@@ -5,24 +5,23 @@ const app = express();
 
 app.use(express.static('pages'))
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname+'/pages/home/index.html');
-    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    console.log(ip + " requested home page")
-});
-
-app.get('/matchgame', function (req, res) {
-    res.sendFile(__dirname+'/pages/matchgame/index.html');
-	var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    console.log(ip + " requested matchgame");
-});
-
 app.get('/wemoexplorer', function (req, res) {
     res.sendFile(__dirname+'/pages/wemo/wemo.html');
-    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     console.log(ip+" requested wemo");
 });
 
+app.get('/', function (req, res) {
+    res.sendFile(__dirname+'/pages/home.html');
+    let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    console.log(ip+" requested home page");
+});
+
+app.get('/matchgame', function (req, res) {
+    res.sendFile(__dirname+'/pages/matchgame/matchgame.html');
+	let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    console.log(ip+" requested matchgame");
+});
 
 
 app.listen(PORT, () => console.log(`${new Date().toLocaleTimeString()}: Listening on ${ PORT }`))
